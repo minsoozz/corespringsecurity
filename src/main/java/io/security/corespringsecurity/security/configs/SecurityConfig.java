7c46f1ad3c7441371b861ad426a4239638775bec
minsoozz <minsoo1604@naver.com>
2021-07-21 23:52:31 +0900

    4) DB 연동 인증 처리(1) : CustomUserDetailsService

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,5 +1,6 @@
 package io.security.corespringsecurity.security.configs;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -9,21 +10,27 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
-@Slf4j
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+  private final UserDetailsService userDetailsService;
+
+  /**
+   * 스프링 시큐리티가 사용자가 만든 UserDetailsService 구현체를 사용해서 인증처리를 하게 된다
+   *
+   * @param auth
+   * @throws Exception
+   */
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    String password = passwordEncoder().encode("1111");
-    auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
-    auth.inMemoryAuthentication().withUser("manager").password(password).roles("USER", "MANAGER");
-    auth.inMemoryAuthentication().withUser("admin").password(password)
-        .roles("USER", "MANAGER", "ADMIN");
+    auth.userDetailsService(userDetailsService);
   }
 
   @Bean
@@ -33,6 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   /**
    * WebIgnore 설정
+   *
    * @param web
    * @throws Exception
    */
@@ -45,7 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(final HttpSecurity http) throws Exception {
     http
         .authorizeRequests()
-        .antMatchers("/","/users").permitAll()
+        .antMatchers("/", "/users").permitAll()
         .antMatchers("/mypage").hasRole("USER")
         .antMatchers("/messages").hasRole("MANAGER")
         .antMatchers("/config").hasRole("ADMIN")
---
+++ src/main/java/io/security/corespringsecurity/sercvice/AccountContext.java
@@ -0,0 +1,21 @@
+package io.security.corespringsecurity.sercvice;
+
+
+import io.security.corespringsecurity.domain.Account;
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+public class AccountContext extends User {
+
+  private final Account account;
+
+  public AccountContext(Account account, Collection<? extends GrantedAuthority> authorities) {
+    super(account.getUsername(), account.getPassword(), authorities);
+    this.account = account;
+  }
+
+  public Account getAccount() {
+    return account;
+  }
+}
