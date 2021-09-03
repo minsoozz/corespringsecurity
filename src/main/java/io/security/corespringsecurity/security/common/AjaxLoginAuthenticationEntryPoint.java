2aa047f890476b9e3cad30ec90cf15643753ecdf
minsoozz <minsoo1604@naver.com>
2021-09-03 18:20:00 +0900

    5) 인증 및 인가 예외 처리 - AjaxLoginUrlAuthenticationEntryPoint, AjaxAccessDeniedHandler
    - 오류 해결 완료
    - ROLE_MANAGER 계정으로 로그인을 하지 않아 401 오류 발생

--- src/main/java/io/security/corespringsecurity/security/common/AjaxLoginAuthenticationEntryPoint.java
+++ src/main/java/io/security/corespringsecurity/security/common/AjaxLoginAuthenticationEntryPoint.java
@@ -9,6 +9,14 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 
 public class AjaxLoginAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+  /**
+   * 클라이언트로부터 인증을 받지 않은 상태로 자원에 접근을 했기 때문에 401 에러코드를 전달한다
+   * @param request
+   * @param response
+   * @param e
+   * @throws IOException
+   * @throws ServletException
+   */
   @Override
   public void commence(HttpServletRequest request,
       HttpServletResponse response, AuthenticationException e)
