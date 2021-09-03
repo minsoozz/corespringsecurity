2cf0a9454acca5248fb70ee707921ed22dee4799
minsoozz <minsoo1604@naver.com>
2021-09-03 19:49:47 +0900

    6) Ajax Custom DSLs 구현하기
    - 강의 진행중

--- src/main/java/io/security/corespringsecurity/security/common/AjaxAccessDeniedHandler.java
+++ src/main/java/io/security/corespringsecurity/security/common/AjaxAccessDeniedHandler.java
@@ -9,6 +9,14 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 
 public class AjaxAccessDeniedHandler implements AccessDeniedHandler {
 
+  /**
+   * 인증은 받았지만 권한이 없는 경우 호출
+   * @param request
+   * @param response
+   * @param e
+   * @throws IOException
+   * @throws ServletException
+   */
   @Override
   public void handle(HttpServletRequest request, HttpServletResponse response,
       AccessDeniedException e) throws IOException, ServletException {
