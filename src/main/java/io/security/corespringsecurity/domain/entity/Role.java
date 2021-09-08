b5e9977f40bd5e23d37f052ceecc6949af4ad838
minsoozz <minsoo1604@naver.com>
2021-09-09 00:34:20 +0900

    8) 계층 권한 적용하기- RoleHierarchy
    - 강의 진행중

--- src/main/java/io/security/corespringsecurity/domain/entity/Role.java
+++ src/main/java/io/security/corespringsecurity/domain/entity/Role.java
@@ -1,25 +1,13 @@
 
 package io.security.corespringsecurity.domain.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.OrderBy;
-import javax.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @Entity
 @Table(name = "ROLE")
--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -10,6 +10,7 @@ import io.security.corespringsecurity.security.metadatasource.UrlFilterInvocatio
 import io.security.corespringsecurity.security.provider.AjaxAuthenticationProvider;
 import io.security.corespringsecurity.security.provider.FormAuthenticationProvider;
 import io.security.corespringsecurity.service.SecurityResourceService;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
@@ -19,7 +20,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.access.vote.AffirmativeBased;
+import org.springframework.security.access.vote.RoleHierarchyVoter;
 import org.springframework.security.access.vote.RoleVoter;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -160,7 +164,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   }
 
   private List<AccessDecisionVoter<?>> getAccessDecistionVoters() {
-    return Arrays.asList(new RoleVoter());
+
+    List<AccessDecisionVoter<? extends Object>> accessDecisionVoters = new ArrayList<>();
+    accessDecisionVoters.add(roleVoter());
+
+    return accessDecisionVoters;
+  }
+
+  @Bean
+  public AccessDecisionVoter<? extends Object> roleVoter() {
+    return new RoleHierarchyVoter(roleHierarchy());
+  }
+
+  @Bean
+  public RoleHierarchyImpl roleHierarchy() {
+    return new RoleHierarchyImpl();
   }
 
   private UrlResourcesMapFactoryBean urlResourcesMapFactoryBean() {
